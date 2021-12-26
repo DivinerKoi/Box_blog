0fe213887540dd0edb8e74c89304582d60b15f26
DivinerKoi <you@example.com>
2021-12-27 04:19:42 +0800

    更新资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,9 +27,29 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-        
+
         Auth::login($user);
         session()->flash('success', '欢迎进入BOX博客，这里将是您的世界！');
         return redirect()->route('users.show',[$user]);
+    }
+
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        
+        $data = [];
+        $data['name'] = $request->name; 
+        if ($request->password) { 
+            $data['password'] = bcrypt($request->password); 
+            }
+            $user->update($data);
+            session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user);
     }
 }
